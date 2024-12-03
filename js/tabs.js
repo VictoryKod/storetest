7f52c44231b09899c7451e59b17ada2e3c234b32
V.Shevchenko <victory.kod6@gmail.com>
2024-12-04 04:53:08 +0500

    add js tabs

---
+++ js/tabs.js
@@ -0,0 +1,15 @@
+
+const tabItem = document.querySelectorAll('.tabs__btn-item'); 
+const tabContent = document.querySelectorAll('.tabs__content-item'); 
+
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
+});
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  tabItem.forEach(function(item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+  tabTarget.classList.add('tabs__btn-item--active');
+}
