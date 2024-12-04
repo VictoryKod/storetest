23421ab9c6fe9e3c220c88a25a90f948486b85a0
V.Shevchenko <victory.kod6@gmail.com>
2024-12-04 08:27:09 +0500

    fix bug tabs.js, add card hover

--- js/tabs.js
+++ js/tabs.js
@@ -4,12 +4,22 @@ const tabContent = document.querySelectorAll('.tabs__content-item');
 
 tabItem.forEach(function(element) {
   element.addEventListener('click', open);
-});
+})
 
 function open(evt) {
   const tabTarget = evt.currentTarget;
-  tabItem.forEach(function(item) {
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function(item){
     item.classList.remove('tabs__btn-item--active');
   });
+
   tabTarget.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach(function(item){
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+
 }
