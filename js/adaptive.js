a7b7669ed5aa81c2ea2bde6f88dcba4927f729bf
V.Shevchenko <victory.kod6@gmail.com>
2024-12-06 04:57:36 +0500

    ad adaptive new, contacts

--- js/adaptive.js
+++ js/adaptive.js
@@ -1,7 +1,8 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu__list');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu__btn');
+    const menu = document.querySelector('.menu__list');
 
-/* Отслеживаем клик, если клик совершился */
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu__list--active');
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('menu__list--active');
+    });
 });
